b4e314a39af6009318d9b49dfd28d744df82e497
Joe Marion <joemarion.io@gmail.com>
2016-10-30 16:06:52 -0700

    Pass all tests with initial code.

--- exercise.rb
+++ exercise.rb
@@ -1,10 +1,15 @@
 class Exercise
 
-  # Assume that "str" is a sequence of words separated by spaces.
-  # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
-  # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
-  def self.marklar(str)
-    # TODO: Implement this method
+  # Takes a string and replaces any words more than 4 chars and replaces word
+  def self.marklar(str, rep = "marklar")
+    str_array = str.split(' ')
+
+    # Creates an array and replaces all words with more than 4 chars
+    transformed_array = str_array.inject([]) do |i, word|
+      word.length > 4 ? i << word_replacer(word, rep) : i << word
+    end
+
+    return transformed_array.join(' ') # Join the array to return the final string
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -13,6 +18,31 @@ class Exercise
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
     # TODO: Implement this method
+    fib_array = []
+    (nth).times do |num|
+      fib_array << fibonacci(num + 1)
+    end
+    fib_array.inject(0) do |i, val|
+      val % 2 == 0 ? i + val : i
+    end
   end
 
+  private
+
+    # Returns True if input is capitalized
+    def self.capitalized(word)
+      word[0] == word[0].upcase
+    end
+
+    # Replaces arg1 with arg2 maintaining capitalization and punctuation
+    def self.word_replacer(word, replacement)
+      punctuation = word.match(/[[:punct:]]/).to_s # Use REGEX to find punctuation
+      rep_word = capitalized(word) ? replacement.capitalize : replacement.downcase # Check for capitalization
+      punctuation.nil? ? rep_word : rep_word + punctuation # Implicit return with punctuation
+    end
+
+    # Fibonacci Sequence
+    def self.fibonacci(n)
+      n <= 1 ? n :  fibonacci( n - 1 ) + fibonacci( n - 2 )
+    end
 end
